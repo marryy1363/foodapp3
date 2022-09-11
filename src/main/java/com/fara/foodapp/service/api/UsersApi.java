7a1e2b4e5b47ac103b0b1b485e0a6f73a10972b7
m.vares <marryy1363@gmail.com>
2022-09-11 06:59:31 -0700

    initial users controler and added error message

--- src/main/java/com/fara/foodapp/service/api/UsersApi.java
+++ src/main/java/com/fara/foodapp/service/api/UsersApi.java
@@ -1,7 +1,19 @@
 package com.fara.foodapp.service.api;
 
+import com.fara.foodapp.domain.Users;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface UsersApi {
+    List<Users> findAllUsers();
+
+    Users findById(Long id) throws Exception;
+
+    Users save(Users users);
+
+    Users edit(Users users) throws Exception;
+
+    void delete(Long id) throws Exception;
 }
