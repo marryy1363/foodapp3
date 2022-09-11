21ba2a6ac70bbf0a3e046e89afc715eb45be9c54
m.vares <marryy1363@gmail.com>
2022-09-10 23:11:12 -0700

    entity class BT_ExecutionState

---
+++ src/main/java/com/fara/foodapp/domain/BT_ExecutionState.java
@@ -0,0 +1,33 @@
+package com.fara.foodapp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "BT_ExecutionState")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BT_ExecutionState {
+
+    @Id
+    @Column(name = "code", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long code;
+
+
+    @NotBlank(message = "title  is required but not provided")
+    @NotEmpty(message = "title is required but not provided")
+    @NotNull(message = "title  is required but not provided")
+    @Column(name = "title", nullable = false)
+    private String title;
+
+}
