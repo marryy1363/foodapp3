f02a251e79c6483a0c9433630fa6f4c15d6fa3c1
m.vares <marryy1363@gmail.com>
2022-09-11 00:13:00 -0700

    entity class BT_PlanState

---
+++ src/main/java/com/fara/foodapp/domain/BT_PlanState.java
@@ -0,0 +1,31 @@
+package com.fara.foodapp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "BT_PlanState")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BT_PlanState {
+    @Id
+    @Column(name = "code", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long code;
+
+
+    @NotBlank(message = "title  is required but not provided")
+    @NotEmpty(message = "title is required but not provided")
+    @NotNull(message = "title  is required but not provided")
+    @Column(name = "title", nullable = false)
+    private String title;
+}
