85f58e47f7927bc99980a4e75f9ef73eb0d92653
m.vares <marryy1363@gmail.com>
2022-09-11 00:53:45 -0700

    entity class TB_Supplier

---
+++ src/main/java/com/fara/foodapp/domain/TB_Supplier.java
@@ -0,0 +1,31 @@
+package com.fara.foodapp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "TB_Supplier")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TB_Supplier {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    @NotBlank(message = "title  is required but not provided")
+    @NotEmpty(message = "title is required but not provided")
+    @NotNull(message = "title  is required but not provided")
+    @Column(name = "title", nullable = false)
+    private String title;
+}
