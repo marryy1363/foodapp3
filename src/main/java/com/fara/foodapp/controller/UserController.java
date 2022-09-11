a36ad2f90d3b4329a186f497eb3a55659c35f037
m.vares <marryy1363@gmail.com>
2022-09-11 06:59:31 -0700

    initial users controler

---
+++ src/main/java/com/fara/foodapp/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.fara.foodapp.controller;
+
+import com.fara.foodapp.domain.Users;
+import com.fara.foodapp.service.api.UsersApi;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/food-app/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private UsersApi usersApi;
+
+
+    @GetMapping("/all")
+    List<Users> findAll() {
+        return usersApi.findAllUsers();
+    }
+
+
+    @GetMapping("/find-by-id/{id}")
+    Users findById(@PathVariable Long id) throws Exception {
+        return usersApi.findById(id);
+
+    }
+
+
+
+    @PostMapping("/save")
+    Users save(@RequestBody @Valid Users users){
+        return usersApi.save(users);
+
+    }
+
+    @PostMapping("/Edit")
+    void Edit(@RequestBody @Valid Users users) throws Exception {
+        usersApi.edit(users);
+
+    }
+
+    @GetMapping("/delete/{id}")
+    void delete(@PathVariable Long id) throws Exception {
+        usersApi.delete(id);
+    }
+}
--- src/main/java/com/fara/foodapp/service/api/UsersImpl.java
+++ src/main/java/com/fara/foodapp/service/api/UsersImpl.java
@@ -1,7 +1,52 @@
 package com.fara.foodapp.service.api;
 
+import com.fara.foodapp.domain.Users;
+import com.fara.foodapp.repo.UsersRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class UsersImpl implements UsersApi {
+    UsersRepository usersRepository;
+    @Override
+    public List<Users> findAllUsers() {
+        return usersRepository.findAll();
+    }
+
+    @Override
+    public Users findById(Long id) throws Exception {
+        Optional<Users> byId = usersRepository.findById(id);
+        if (byId.isPresent())
+            return byId.get();
+        else
+            throw new Exception("user not found.");
+
+    }
+
+    @Override
+    public Users save(Users users) {
+        return usersRepository.save(users);
+    }
+
+    @Override
+    public Users edit(Users users) throws Exception {
+        if (usersRepository.findById(users.getId()).isPresent())
+            return usersRepository.save(users);
+        else
+            throw new Exception("id not found");
+
+    }
+
+    @Override
+    public void delete(Long id) throws Exception {
+        Optional<Users> byId = usersRepository.findById(id);
+        if (byId.isPresent())
+            usersRepository.delete(byId.get());
+        else
+            throw new Exception("id not found");
+    }
 }
