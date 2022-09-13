605bc2ca13373c32d3c0af305a86d0cab1daa49a
m.vares <marryy1363@gmail.com>
2022-09-13 07:27:05 -0700

    solving useing json for login second result

--- src/main/java/com/fara/foodapp/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/fara/foodapp/filter/CustomAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.fara.foodapp.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fara.foodapp.domain.Users;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +12,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.util.StreamUtils;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -38,20 +40,28 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
 
         try {
-            BufferedReader reader = request.getReader();
-            StringBuilder sb = new StringBuilder();
-            String line = reader.readLine();
-            while (line != null) {
-                sb.append(line + "\n");
-                line = reader.readLine();
-            }
-            reader.close();
-            String username = sb.substring(16,22);
-            String password = sb.substring(36,45);
+//            BufferedReader reader = request.getReader();
+//            StringBuilder sb = new StringBuilder();
+//            String line = reader.readLine();
+//            while (line != null) {
+//                sb.append(line + "\n");
+//                line = reader.readLine();
+//            }
+//            reader.close();
+//            String username = sb.substring(16,22);
+//            String password = sb.substring(36,45);
 
+            byte[] inputStreamBytes = StreamUtils.copyToByteArray(request.getInputStream());
+            Map<String, String> jsonRequest = new ObjectMapper().readValue(inputStreamBytes, Map.class);
+//            Users creds = new ObjectMapper().readValue(jsonRequest.get("body"), Users.class);
+            String userId = jsonRequest.get("userId");
+            String pwd = jsonRequest.get("pwd");
+//
+//            String username = creds.getUserId();
+//            String password  = creds.getPwd();
 
-            log.info("username is {}",username);log.info("pass is {}",password);
-            UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(username,password);
+//            log.info("username is {}",username);log.info("pass is {}",password);
+            UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(userId,pwd);
             return authenticationManager.authenticate(authenticationToken);
         } catch (IOException e) {
             throw new RuntimeException(e);
